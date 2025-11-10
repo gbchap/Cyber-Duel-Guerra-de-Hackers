089277812bfe705beb8d27f6ee8287bc82391e3e
gbchap <gb.freitaschap@gmail.com>
2025-11-10 16:32:48 -0300

    entrada

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // carregando as cartas:
 
-        GerenciadorCartas gerenciador = new GerenciadorCartas();
-        gerenciador.carregarCartas();
-        System.out.println("Total de cartas carregadas: " + gerenciador.getTotalCartas());
+        //GerenciadorCartas gerenciador = new GerenciadorCartas();
+        //gerenciador.carregarCartas();
+        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Bem-vindo, jogador. Digite suas informações.");
+        System.out.print("Digite seu nome: ");
+        String nome = entrada.nextLine(); 
+
     }
 }
