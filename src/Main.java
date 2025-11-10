b14c6d37e7b8e5ed5ad41c9eeeb5a282081a1e57
gbchap <gb.freitaschap@gmail.com>
2025-11-10 16:35:36 -0300

    uuauaua

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,14 @@ public class Main {
         //GerenciadorCartas gerenciador = new GerenciadorCartas();
         //gerenciador.carregarCartas();
         
-        Scanner entrada = new Scanner(System.in);
+        Scanner entrada1 = new Scanner(System.in);
 
         System.out.println("Bem-vindo, jogador. Digite suas informações.");
         System.out.print("Digite seu nome: ");
-        String nome = entrada.nextLine(); 
+        String nome1 = entrada1.nextLine(); 
+        System.out.print("Digite seu identificador (número de matrícula): ");
+        String id1 = entrada1.nextLine();
 
+        entrada1.close();
     }
 }
