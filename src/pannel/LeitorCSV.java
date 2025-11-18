ffd983bc310197c7f4254b72dd099f845d32eeac
Dyanna Azevedo <dyvazevedo@gmail.com>
2025-11-18 13:04:50 -0300

    Adiciona LeitorCSV (arquivo novo)

---
+++ src/pannel/LeitorCSV.java
@@ -0,0 +1,116 @@
+package src.pannel;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import src.data.CartaP;
+import src.data.CartaSup;
+
+public class LeitorCSV {
+
+    //ATAQUE 
+    public static ArrayList<CartaP> lerAtaque(String caminho) {
+
+        ArrayList<CartaP> lista = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+            String linha;
+            boolean cabecalho = true;
+
+            while ((linha = br.readLine()) != null) {
+
+                if (cabecalho) { 
+                    cabecalho = false; 
+                    continue; 
+                }
+
+                String[] p = linha.split(",");
+
+                String nome = p[0];
+                String tipo = p[1];
+                int poder = Integer.parseInt(p[2]);
+                int custo = Integer.parseInt(p[3]);
+                String descricao = p[4];
+
+                lista.add(new CartaP(nome, tipo, poder, custo, descricao));
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro ao ler ataque.csv: " + e.getMessage());
+        }
+
+        return lista;
+    }
+
+
+    //DEFESA 
+    public static ArrayList<CartaP> lerDefesa(String caminho) {
+
+        ArrayList<CartaP> lista = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+            String linha;
+            boolean cabecalho = true;
+
+            while ((linha = br.readLine()) != null) {
+
+                if (cabecalho) { 
+                    cabecalho = false; 
+                    continue; 
+                }
+
+                String[] p = linha.split(",");
+
+                String nome = p[0];
+                String tipo = p[1];
+                int poder = Integer.parseInt(p[2]);
+                int custo = Integer.parseInt(p[3]);
+                String descricao = p[4];
+
+                lista.add(new CartaP(nome, tipo, poder, custo, descricao));
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro ao ler defesa.csv: " + e.getMessage());
+        }
+
+        return lista;
+    }
+
+
+    //SUPORTE
+    public static ArrayList<CartaSup> lerSuporte(String caminho) {
+
+        ArrayList<CartaSup> lista = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+
+            String linha;
+            boolean cabecalho = true;
+
+            while ((linha = br.readLine()) != null) {
+
+                if (cabecalho) { 
+                    cabecalho = false; 
+                    continue; 
+                }
+
+                String[] p = linha.split(",");
+
+                String nome = p[0];
+                String tipo = p[1];
+                double poder = Double.parseDouble(p[2]); // 0.2
+                int custo = Integer.parseInt(p[3]);
+                String efeito = p[4];
+                String descricao = p[5];
+
+                lista.add(new CartaSup(nome, tipo, poder, custo, efeito, descricao));
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro ao ler suporte.csv: " + e.getMessage());
+        }
+
+        return lista;
+    }
+}
