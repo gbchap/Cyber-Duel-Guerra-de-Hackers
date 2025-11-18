6c01ce0164368310498e69c9580d16f708dc37f8
Dyanna Azevedo <dyvazevedo@gmail.com>
2025-11-18 01:15:49 -0300

    leitura de CSV e ajustes

--- src/data/CartaSup.java
+++ src/data/CartaSup.java
@@ -3,8 +3,8 @@ package src.data;
 public class CartaSup extends CartaP {
     private String efeito;
 
-    public CartaSup(String nome, int tipo, int poder, String efeito, String descricao) {
-        super(nome, "SUPORTE", poder, efeito, descricao); 
+     public CartaSup(String nome, String tipo, double poder, int custo, String efeito, String descricao) {
+        super(nome, "SUPORTE", (int) poder, custo, descricao);
         this.efeito = efeito;
     }
     public String getEfeito() {
--- src/pannel/Main.java
+++ src/pannel/Main.java
@@ -1,23 +1,52 @@
 package src.pannel;
 
-import hackers.Jogador;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import src.data.CartaP;
+import src.data.CartaSup;
+import src.hackers.Jogador;
 
 public class Main {
     public static void main(String[] args){
-        // carregando as cartas:
 
+       
+        //CARREGAR CARTAS DO CSV
+        System.out.println("Carregando banco de cartas...");
 
-       System.out.println("Bem-vindo, jogador. Digite suas informações.");
-        
+        ArrayList<CartaP> ataques =
+            LeitorCSV.lerAtaque("src/data/ataque.csv");
+
+        ArrayList<CartaP> defesas =
+            LeitorCSV.lerDefesa("src/data/defesa.csv");
+
+        ArrayList<CartaSup> suportes =
+            LeitorCSV.lerSuporte("src/data/suporte.csv");
+
+        System.out.println("Cartas carregadas:");
+        System.out.println("Ataques: " + ataques.size());
+        System.out.println("Defesas: " + defesas.size());
+        System.out.println("Suportes: " + suportes.size());
+        // TESTE impressão da leitura
+                System.out.println("\nPrimeiro ataque:");
+                ataques.get(0).get();
+
+                System.out.println("\nPrimeira defesa:");
+                defesas.get(0).get();
+
+                System.out.println("\nPrimeiro suporte:");
+                suportes.get(0).get();
+
+        System.out.println("\nBem-vindo, jogador. Digite suas informa��es.");
+
         Scanner entrada = new Scanner(System.in);
 
         Jogador hacker1 = new Jogador();
         Jogador hacker2;
         hacker1.coletarInfo(entrada);
-        
+
         System.out.print("Seu competidor será um robô (0) ou um humano? (1): ");
-        
+
         String robo_humano = entrada.nextLine();
         while (!robo_humano.equals("0") && !robo_humano.equals("1")){
             System.out.print("Opção Inválida! Digite Opção Válida: ");
